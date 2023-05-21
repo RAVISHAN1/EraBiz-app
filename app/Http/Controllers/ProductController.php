0ffca40f2f07cfee77613a3c16d496ab7d5f34e3
janith-emedia <janithr.emedia@gmail.com>
2023-05-21 16:33:27 +0530

    Products filter

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,15 +4,45 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Product::all();
+        $query = Product::query();
+
+        // search by name
+        if ($request->has('name')) {
+            $name = $request->name;
+            $query->where(function ($q) use ($name) {
+                $q->where('name', 'LIKE', "%$name%");
+            });
+        }
+
+        // filter by min price
+        if ($request->has('min_price')) {
+            $min_price = $request->min_price;
+            $query->where(function ($q) use ($min_price) {
+                $q->where('price', '>=', $min_price);
+            });
+        }
+
+        // filter by max price
+        if ($request->has('max_price')) {
+            $max_price = $request->max_price;
+            $query->where(function ($q) use ($max_price) {
+                $q->where('price', '<=', $max_price);
+            });
+        }
+
+        $products = $query->get();
+
+        return response()->json($products);
     }
 
     /**
@@ -20,8 +50,38 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        $product = Product::create($request->all());
-        return response()->json($product, 201);
+        try {
+            $data = $request->all();
+            $validator = Validator::make($data, [
+                'name'          => 'required|string',
+                'description'   => 'required|string',
+                'price'         => 'required|numeric',
+                'image_url'     => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], 422);
+            }
+
+            // save image
+            if ($request->hasFile('image_url')) {
+                $image = $request->file('image_url');
+                $imageName = time() . '.' . $image->getClientOriginalExtension();
+                $url = $image->move(public_path('images'), $imageName);
+
+                // get image url
+                $data['image_url'] = $url;
+            }
+
+            $product = Product::create($data);
+
+            return response()->json($product->refresh(), 201);
+        } catch (\Throwable $th) {
+            // Log the exception
+            Log::error('An error occurred while storing a product | ', ['exception' => $th->getMessage()]);
+            // Handle the exception
+            return response()->json(['error' => 'An error occurred'], 500);
+        }
     }
 
     /**
